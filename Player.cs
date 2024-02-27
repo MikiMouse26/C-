7744b40fb1a55d7e53a32a6297ac4136ae8a6c2a
Макс <161010960+MikiMouse26@users.noreply.github.com>
2024-02-27 18:04:43 +0500

    Create Player.cs

---
+++ Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0f);
+        rb.velocity = movement * moveSpeed;
+    }
